e3d13824207aecb09b05858e1e7cddac6e5a1cd4
gigasamkharadze <samkharadze.giga@kiu.edu.ge>
2024-07-09 22:08:36 +0400

    scroll to top when route changes

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,13 @@ import routes from './routes'
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition
+        } else {
+            return {top: 0}
+        }
+    }
 })
 
 export default router
