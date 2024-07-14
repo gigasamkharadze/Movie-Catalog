d037af8a7d31537b89877b8b87d475c5e5ef1168
gigasamkharadze <samkharadze.giga@kiu.edu.ge>
2024-07-15 01:22:27 +0400

    rate movies with 1-5 stars on the detailed view

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -5,6 +5,7 @@ import moviesApiService from "../services/api/moviesApiService.js";
 import ImageCarousel from "../components/ImageCarousel.vue";
 import MainMovieCard from "../components/MainMovieCard.vue";
 import {useMoviesStore} from "../store/movies.js";
+import MovieRaterStarts from "../components/shared/MovieRaterStarts.vue";
 
 const route = useRoute();
 const apiService = new moviesApiService();
@@ -48,7 +49,6 @@ const getSimilarMovies = async () => {
 
 const addToFavorites = () => {
   moviesStore.addToFavorites(movie.id);
-  addedToFavorites.value = true;
 }
 
 watch(() => route.params.id, () => {
@@ -68,29 +68,41 @@ onMounted(() => {
 <template>
   <div>
     <div class="flex flex-col w-9/12 mx-auto p-6 gap-3 shadow-2xl mb-10">
-      <ImageCarousel :images="images.backdrops" />
+      <ImageCarousel :images="images.backdrops"/>
       <div>
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{movie.title}}</h5>
+        <div class="flex justify-between">
+          <h2 class="mb-2 text-3xl font-bold tracking-tight text-gray-900">{{ movie.title }}</h2>
+          <MovieRaterStarts/>
+        </div>
         <p class="mb-3 first-letter:text-5xl first-letter:font-bold first-letter:text-gray-900 first-letter:me-3
-                  first-letter:float-start">{{movie.overview}}</p>
+                  first-letter:float-start">{{ movie.overview }}</p>
         <div class="grid grid-cols-2 gap-y-4 gap-x-2 p-4 bg-white rounded-lg shadow-md">
           <span class="text-gray-700">Genres:</span>
           <div class="flex flex-wrap space-x-2">
             <span v-for="genre in movie.genres"
                   :key="genre.id"
-                  class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded"> {{genre.name}} </span>
+                  class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded"> {{
+                genre.name
+              }} </span>
           </div>
           <span class="text-gray-700">Country:</span>
           <div class="flex flex-wrap space-x-2">
             <span v-for="country in movie.production_countries"
                   :key="country.id"
-                  class="bg-green-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded"> {{country.name}} </span>
+                  class="bg-green-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded"> {{
+                country.name
+              }} </span>
           </div>
           <span class="text-gray-700">Release Date:</span>
-          <div class="w-fit bg-teal-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded">{{movie.release_date}}</div>
+          <div class="w-fit bg-teal-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded">
+            {{ movie.release_date }}
+          </div>
           <span class="text-gray-700">Production:</span>
           <div class="flex flex-wrap space-x-2">
-            <span v-for="production in movie.production_companies" :key="production.id" class="bg-purple-100 text-purple-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded"> {{production.name}} </span>
+            <span v-for="production in movie.production_companies" :key="production.id"
+                  class="bg-purple-100 text-purple-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded"> {{
+                production.name
+              }} </span>
           </div>
         </div>
         <button
@@ -108,7 +120,7 @@ onMounted(() => {
         <MainMovieCard v-for="movie in similarMovies.results"
                        class="min-w-[300px]"
                        :key="movie.id"
-                       :movie="movie" />
+                       :movie="movie"/>
       </div>
     </div>
   </div>
