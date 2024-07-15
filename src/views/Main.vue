a4c8839d3db28b6a8bb47b0de5759c5ed91d2a07
gigasamkharadze <samkharadze.giga@kiu.edu.ge>
2024-07-15 23:14:13 +0400

    loader when movies are being fetched

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,3 +1,4 @@
+
 <script setup>
 import {computed, onMounted, ref} from "vue";
 import MainMovieCard from "../components/MainMovieCard.vue";
@@ -10,8 +11,10 @@ const movies = computed(() => moviesStore.movies);
 const totalPages = 500;
 const currentPage = ref(1);
 const title = ref("");
+const isLoading = ref(true); // Added loading state
 
 const onPageChange = async (page) => {
+  isLoading.value = true;
   currentPage.value = page;
   window.scrollTo({
     top: 0,
@@ -22,16 +25,21 @@ const onPageChange = async (page) => {
   } else {
     await moviesStore.getMovies(currentPage.value);
   }
+  isLoading.value = false;
 };
 
 const getMovies = async (page) => {
+  isLoading.value = true;
   await moviesStore.getMovies(page);
+  isLoading.value = false;
 };
 
 const searchMovies = async (movieTitle) => {
+  isLoading.value = true;
   currentPage.value = 1;
   title.value = movieTitle;
   await moviesStore.getMoviesByTitle(movieTitle, currentPage.value);
+  isLoading.value = false;
 };
 
 onMounted(() => getMovies(currentPage.value));
@@ -40,10 +48,18 @@ onMounted(() => getMovies(currentPage.value));
 <template>
   <div class="px-12">
     <SharedSearchBar @search="searchMovies"/>
-    <div class="grid grow gap-x-4 gap-y-8 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    <div v-if="isLoading" class="h-16 flex justify-center items-center gap-4">
+      <img
+          class="w-6 h-6 animate-spin"
+          src="../assets/loading.svg"
+          alt="Loading...">
+      <span class="text-xl">Loading movies...</span>
+    </div>
+    <div v-else class="grid grow gap-x-4 gap-y-8 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       <MainMovieCard v-for="movie in movies" :key="movie.id" :movie="movie"/>
     </div>
-    <PagePaginationBar @page-change="onPageChange"
+    <PagePaginationBar v-if="!isLoading"
+                        @page-change="onPageChange"
                        :current-page="currentPage"
                        :total-pages="totalPages"/>
   </div>
